790a5cc9ae86a37b0d232bf4702453703d50ab94
MEDI SIM <mehdi.mu1902@gmail.com>
2026-03-28 18:45:52 +0000

    station_dimension model creation

---
+++ models/dbt_tuto/station_dimension.sql
@@ -0,0 +1,11 @@
+with BIKE as (
+
+select distinct
+start_statio_id as station_id,
+start_station_name as station_name,
+start_lat as station_lat,
+start_lng as station_log
+from {{ source('demo', 'bike') }}
+)
+
+select * from BIKE
