0eb13c491ae886dbc7b3f3c8721791d1bab96d98
MEDI SIM <mehdi.mu1902@gmail.com>
2026-03-31 15:24:56 +0000

    test

--- models/example/test.sql
+++ models/example/test.sql
@@ -1,5 +1,5 @@
 /*select * from {{ source('demo', 'weather') }};
 
-select * from {{ source('demo', 'bike') }};
+select * from {{ source('demo', 'bike') }};*/
 
-select * from {{ ref('my_second_dbt_model') }};*/
+select * from {{ ref('my_second_dbt_model') }};
