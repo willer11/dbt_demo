9b536942e73a5108057e6435252941146a4030d0
MEDI SIM <mehdi.mu1902@gmail.com>
2026-03-31 15:29:21 +0000

    correction test.sql model file

--- models/example/test.sql
+++ models/example/test.sql
@@ -1,5 +1,8 @@
 /*select * from {{ source('demo', 'weather') }};
 
 select * from {{ source('demo', 'bike') }};*/
+with CTE AS (
+select * from {{ ref('my_second_dbt_model') }}
+)
 
-select * from {{ ref('my_second_dbt_model') }};
+select * from CTE
